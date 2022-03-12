dbdabddd9ee35388d6b3e8b8a4a66da94b786f56
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-12 20:07:18 +0000

    update max_updates and segmentation script

--- segmentation/fairseq_segment.sh
+++ segmentation/fairseq_segment.sh
@@ -6,31 +6,30 @@ decode() {
     local -r MODE="$1"; shift
     # Fairseq insists on calling the dev-set "valid"; hack around this.
     local -r FAIRSEQ_MODE="${MODE/dev/valid}"
-    for CHECKPOINT in $(ls ${CP}/checkpoint[1-9]*.pt 2> /dev/null); do
-        RES="${CHECKPOINT/.pt/-${MODE}.res}"
-        # Don't overwrite an existing prediction file.
-        if [[ -f "${RES}" ]]; then
-            continue
-        fi
-        echo "Evaluating into ${RES}"
-        OUT="${CP}/${MODE}.out"
-        PRED="${CP}/${MODE}.pred"
-        # Makes raw predictions.
-        fairseq-generate \
-            "data-bin/${NAME}" \
-            --source-lang="${NAME}.src" \
-            --target-lang="${NAME}.tgt" \
-            --path="${CHECKPOINT}" \
-            --seed="${SEED}" \
-            --gen-subset="${FAIRSEQ_MODE}" \
-            --beam="${BEAM}" \
-            --no-progress-bar \
-            > "${OUT}"
-        # Extracts the predictions into a TSV file.
-        cat "${OUT}" | grep '^H-' | cut -f3 > $PRED
-        # Applies the evaluation script to the TSV file.
-        python evaluate.py "${DATA}.dev.tsv" "${PRED}"
-    done
+    CHECKPOINT="fairseq-checkpoints/baseline/checkpoint_best.pt"
+    RES="${CHECKPOINT/.pt/-${MODE}.res}"
+    # Don't overwrite an existing prediction file.
+    if [[ -f "${RES}" ]]; then
+        continue
+    fi
+    echo "Evaluating into ${RES}"
+    OUT="${CP}/${MODE}.out"
+    PRED="${CP}/${MODE}.pred"
+    # Makes raw predictions.
+    fairseq-generate \
+        "data-bin/${NAME}" \
+        --source-lang="${NAME}.src" \
+        --target-lang="${NAME}.tgt" \
+        --path="${CHECKPOINT}" \
+        --seed="${SEED}" \
+        --gen-subset="${FAIRSEQ_MODE}" \
+        --beam="${BEAM}" \
+        --no-progress-bar \
+        > "${OUT}"
+    # Extracts the predictions into a TSV file.
+    cat "${OUT}" | grep '^H-' | cut -f3 > $PRED
+    # Applies the evaluation script to the TSV file.
+    python evaluate.py "${DATA}.dev.tsv" "${PRED}"
 }
 
 decode "fairseq-checkpoints/baseline" dev
