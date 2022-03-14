13d27c7d7f79e294a26a1d2c9d6f02a2898c9909
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-14 11:35:55 +0000

    make fairseq_segment way faster

--- segmentation/fairseq_segment.sh
+++ segmentation/fairseq_segment.sh
@@ -19,16 +19,17 @@ decode() {
     OUT="${CP}/${MODE}.out"
     PRED="${CP}/${MODE}.pred"
     # Makes raw predictions.
-    fairseq-interactive \
+    fairseq-generate \
         "data-bin/${NAME}" \
-        --buffer-size 256 \
         --source-lang="${NAME}.src" \
         --target-lang="${NAME}.tgt" \
         --path="${CHECKPOINT}" \
+        --gen-subset="${FAIRSEQ_MODE}" \
         --beam="${BEAM}" \
-        < "dev.${NAME}.src" > "${OUT}"
+        --no-progress-bar \
+        > "${OUT}"
     # Extracts the predictions into a TSV file.
-    cat "${OUT}" | grep '^H-' | cut -f3 | python postprocess_fairseq.py > $PRED
+    cat "${OUT}" | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | python postprocess_fairseq.py > $PRED
     # Applies the evaluation script to the TSV file.
     python evaluate.py "${DATA}.dev.tsv" "${PRED}"
 }
