73bd8445427292cf87910ed7f789fd644fe45eac
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-15 01:27:06 +0000

    add separate preps for inflection, derivation, compounding

---
+++ segmentation/fairseq_prep_inflection.sh
@@ -0,0 +1,46 @@
+readonly DATA=$1  # example: 2022-shared-tasks/data/eng.word
+NAME=$( basename $DATA ).inflection  # i.e. eng.word
+
+tsv() {
+    for TASK in train dev ; do
+        for TSV in "${DATA}.${TASK}.tsv"; do
+            # Separates graphemes with spaces.
+            grep "1[01][01]" "${TSV}" | cut -f 1 | \
+                sed 's/./& /g' \
+                > "${TASK}.${NAME}".src
+            # segments are a little more complicated here.
+            # damn I'd rather do this in python
+            grep "1[01][01]" "${TSV}" | cut -f2 | \
+                python tokenize_segments.py > "${TASK}.${NAME}".tgt
+        done
+    done
+    TSV="${DATA}.dev.tsv"
+    TASK=test  # pseudotest
+    # Separates graphemes with spaces.
+    cat "${TSV}" | cut -f 1 | \
+        sed 's/./& /g' \
+        > "${TASK}.${NAME}".src
+    # segments are a little more complicated here.
+    # damn I'd rather do this in python
+    cat "${TSV}" | cut -f2 | \
+        python tokenize_segments.py > "${TASK}.${NAME}".tgt
+}
+
+bin() {
+    # todo: fix testpref when it is available
+    fairseq-preprocess \
+        --srcdict="data-bin/eng.word/dict.eng.word.src.txt" \
+        --tgtdict="data-bin/eng.word/dict.eng.word.tgt.txt" \
+        --source-lang="${NAME}.src" \
+        --target-lang="${NAME}.tgt" \
+        --trainpref=train \
+        --validpref=dev \
+        --testpref=test \
+        --tokenizer=space \
+        --thresholdsrc=1 \
+        --thresholdtgt=1 \
+        --destdir="data-bin/${NAME}"
+}
+
+tsv
+bin
