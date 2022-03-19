f3a29c73e7c3a578351f28d6f8052a9d5468ff5c
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-19 10:48:13 +0000

    update fairseq generation script

--- segmentation/fairseq_segment.sh
+++ segmentation/fairseq_segment.sh
@@ -1,4 +1,4 @@
-readonly DATA=$1  # example: 2022-shared-tasks/segmentation/eng.word
+readonly DATA=$1  # example: 2022-shared-tasks/data/eng.word
 NAME=$( basename $DATA )  # i.e. eng.word
 readonly MODEL_PATH=$2
 readonly ENTMAX_ALPHA=$3
@@ -10,7 +10,7 @@ decode() {
     local -r MODE="$1"; shift
     # Fairseq insists on calling the dev-set "valid"; hack around this.
     local -r FAIRSEQ_MODE="${MODE/dev/valid}"
-    CHECKPOINT="${CP}/checkpoint_last.pt"
+    CHECKPOINT="${CP}/checkpoint_best.pt"
     RES="${CHECKPOINT/.pt/-${MODE}.res}"
     # Don't overwrite an existing prediction file.
     if [[ -f "${RES}" ]]; then
@@ -28,12 +28,12 @@ decode() {
         --gen-subset="${FAIRSEQ_MODE}" \
         --beam="${BEAM}" \
         --alpha="${ENTMAX_ALPHA}" \
-        --no-progress-bar \
         > "${OUT}"
     # Extracts the predictions into a TSV file.
     cat "${OUT}" | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | python postprocess_fairseq.py > $PRED
+    cut -f 1 "${DATA}.dev.tsv" | paste - $PRED > "${CP}/${MODE}.guess"
     # Applies the evaluation script to the TSV file.
-    python evaluate.py "${DATA}.dev.tsv" "${PRED}"
+    python evaluate.py 2022SegmentationST/evaluation/ --gold "${DATA}.dev.tsv" --guess "${CP}/${MODE}.guess" --category > "${CP}/${MODE}.results"
 }
 
 decode $MODEL_PATH dev
