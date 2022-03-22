b0f225b37267bc02cd4340885a7448eb699e1ffa
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-22 16:45:27 +0000

    script to finish missing spanish configs

---
+++ segmentation/finish_spa.sh
@@ -0,0 +1,70 @@
+# "fairseq-checkpoints/${NAME}-entmax-${EMB}-${HID}-${LAYERS}-${DROPOUT}-${BATCH}-${ENTMAX_ALPHA}"
+
+
+#spa.word-entmax-512-512-2-0.3-512-1.5
+bash fairseq_train_entmax.sh 512 512 2 0.3 512 1.5
+
+#spa.word-entmax-512-512-2-0.3-256-2
+bash fairseq_train_entmax.sh 512 512 2 0.3 256 2
+
+#spa.word-entmax-512-512-2-0.3-512-2
+bash fairseq_train_entmax.sh 512 512 2 0.3 512 2
+
+#spa.word-entmax-512-512-2-0.5-256-1.5
+bash fairseq_train_entmax.sh 512 512 2 0.5 256 1.5
+
+#spa.word-entmax-512-512-2-0.5-512-1.5
+bash fairseq_train_entmax.sh 512 512 2 0.5 512 1.5
+
+#spa.word-entmax-512-512-2-0.5-256-2
+bash fairseq_train_entmax.sh 512 512 2 0.5 256 2
+
+#spa.word-entmax-512-512-2-0.5-512-2
+bash fairseq_train_entmax.sh 512 512 2 0.5 512 2
+
+#spa.word-entmax-512-1024-1-0.3-256-1.5
+bash fairseq_train_entmax.sh 512 1024 1 0.3 256 1.5
+
+#spa.word-entmax-512-1024-1-0.3-512-1.5
+bash fairseq_train_entmax.sh 512 1024 1 0.3 512 1.5
+
+#spa.word-entmax-512-1024-1-0.3-256-2
+bash fairseq_train_entmax.sh 512 1024 1 0.3 256 2
+
+#spa.word-entmax-512-1024-1-0.3-512-2
+bash fairseq_train_entmax.sh 512 1024 1 0.3 512 2
+
+#spa.word-entmax-512-1024-1-0.5-256-1.5
+bash fairseq_train_entmax.sh 512 1024 1 0.5 256 1.5
+
+#spa.word-entmax-512-1024-1-0.5-512-1.5
+bash fairseq_train_entmax.sh 512 1024 1 0.5 512 1.5
+
+#spa.word-entmax-512-1024-1-0.5-256-2
+bash fairseq_train_entmax.sh 512 1024 1 0.5 256 2
+
+#spa.word-entmax-512-1024-1-0.5-512-2
+bash fairseq_train_entmax.sh 512 1024 1 0.5 512 2
+
+#spa.word-entmax-512-1024-2-0.3-256-1.5
+bash fairseq_train_entmax.sh 512 1024 2 0.3 256 1.5
+
+#spa.word-entmax-512-1024-2-0.3-512-1.5
+bash fairseq_train_entmax.sh 512 1024 2 0.3 512 1.5
+
+#spa.word-entmax-512-1024-2-0.3-256-2
+bash fairseq_train_entmax.sh 512 1024 2 0.3 256 2
+#spa.word-entmax-512-1024-2-0.3-512-2
+bash fairseq_train_entmax.sh 512 1024 2 0.3 512 2
+
+#spa.word-entmax-512-1024-2-0.5-256-1.5
+bash fairseq_train_entmax.sh 512 1024 2 0.5 256 1.5
+
+#spa.word-entmax-512-1024-2-0.5-512-1.5
+bash fairseq_train_entmax.sh 512 1024 2 0.5 512 1.5
+
+#spa.word-entmax-512-1024-2-0.5-256-2
+bash fairseq_train_entmax.sh 512 1024 2 0.5 256 2
+
+#spa.word-entmax-512-1024-2-0.5-512-2
+bash fairseq_train_entmax.sh 512 1024 2 0.5 512 2
