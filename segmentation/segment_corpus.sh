4081c50919eeb07fdb275487dc26d1da6cc71706
Ben Peters <bpeters@coli.uni-saarland.de>
2022-03-24 17:44:08 +0000

    improve segment_corpus.sh

--- segmentation/segment_corpus.sh
+++ segmentation/segment_corpus.sh
@@ -1,27 +1,30 @@
-NAME="eng.word"
+MODEL=$1
+IN_CORPUS=$2
+OUT_CORPUS=$3
+NAME=$OUT_CORPUS
 
 # General idea:
 # - get unique types from corpus (applying some kind of length filtering)
 # fix these paths
 # python unique_types.py < other-data/europarl/Europarl.en-hu.en > other-data/train.eng.words.src
-python unique_types.py < other-data/flores/eng.dev > other-data/dev.eng
+python unique_types.py < $IN_CORPUS > $NAME.uniqs.tmp
 
 # - segment these types (this will require preprocessing them first)
 fairseq-interactive \
     data-bin/eng.word \
-    --path fairseq-checkpoints/eng.word-entmax-512-512-2-.3-1.5/checkpoint_last.pt \
+    --path $MODEL \
     --source-lang eng.word.src \
     --target-lang eng.word.tgt \
     --beam 5 \
     --alpha 1.5  \
     --batch-size 256 \
-    --buffer-size 256 < other-data/dev.eng | \
+    --buffer-size 256 < $NAME.uniqs.tmp | \
     grep -P '^H-'  | cut -c 3- | awk -F "\t" '{print $NF}' | \
-    python postprocess_fairseq.py > other-data/dev.eng.segmented
+    python postprocess_fairseq.py > $NAME.uniqs.segmented
 
 # build the dictionary
-sed "s/ //g" other-data/dev.eng | paste - other-data/dev.eng.segmented > other-data/segment_dict.tsv
-python segment_table.py other-data/segment_dict.tsv < other-data/flores/eng.dev > other-data/flores.eng.segmented
+sed "s/ //g" $NAME.uniqs.tmp | paste - $NAME.uniqs.segmented > $NAME.segment_dict.tsv
+python segment_table.py $NAME.segment_dict.tsv < $IN_CORPUS > $OUT_CORPUS
 
 # is it feasible to do this on a training set? I believe so. The English europarl
 # set has 75k unique types (not too different from the task dev set. The
