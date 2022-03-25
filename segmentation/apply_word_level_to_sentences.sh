bd6dde65c1896fa8b3654e1b64cdcf77cec1fda7
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-25 16:22:46 +0000

    make applying word-level to sentence-level corpus language-general

--- segmentation/apply_word_level_to_sentences.sh
+++ segmentation/apply_word_level_to_sentences.sh
@@ -1,34 +1,37 @@
-WORD_MODEL="fairseq-checkpoints/eng.word-entmax-512-1024-2-0.3-256-1.5/checkpoint_best.pt"
 
-DATA="2022SegmentationST/data/eng.sentence"
+
+WORD_MODEL=$1
+DATA=$2
+NAME=$( basename $DATA )
+LANG=$( basename $DATA .sentence)
 
 # General idea:
 # - get unique types from corpus (applying some kind of length filtering)
 # fix these paths
 # python unique_types.py < other-data/europarl/Europarl.en-hu.en > other-data/train.eng.words.src
 
-cut -f 1 "${DATA}.dev.tsv" | python unique_types.py | sed 's/./& /g' > eng.sentence.dev.src
+cut -f 1 "${DATA}.dev.tsv" | python unique_types.py | sed 's/./& /g' > $NAME.dev.src
 
 # - segment these types (this will require preprocessing them first)
 fairseq-interactive \
     data-bin/eng.word \
     --path $WORD_MODEL \
-    --source-lang eng.word.src \
-    --target-lang eng.word.tgt \
+    --source-lang $LANG.word.src \
+    --target-lang $LANG.word.tgt \
     --beam 5 \
     --alpha 1.5  \
     --batch-size 256 \
-    --buffer-size 256 < eng.sentence.dev.src | \
+    --buffer-size 256 < $NAME.dev.src | \
     grep -P '^H-'  | cut -c 3- | awk -F "\t" '{print $NF}' | \
-    python postprocess_fairseq.py > eng.sentence.dev.values
+    python postprocess_fairseq.py > $NAME.dev.values
 
 # build the dictionary
-sed "s/ //g" eng.sentence.dev.src | paste - eng.sentence.dev.values > eng.sentence.dev.dict
-cut -f 1 "${DATA}.dev.tsv" | python segment_table.py eng.sentence.dev.dict > eng.sentence.dev.pred
+sed "s/ //g" $NAME.dev.src | paste - $NAME.dev.values > $NAME.dev.dict
+cut -f 1 "${DATA}.dev.tsv" | python segment_table.py $NAME.dev.dict > $NAME.dev.pred
 
-cut -f 1 "${DATA}.dev.tsv" | paste - eng.sentence.dev.pred > eng.sentence.guess
+cut -f 1 "${DATA}.dev.tsv" | paste - $NAME.dev.pred > $NAME.guess
 
-python 2022SegmentationST/evaluation/evaluate_word.py --gold $DATA.dev.tsv --guess eng.sentence.guess
+python 2022SegmentationST/evaluation/evaluate_word.py --gold $DATA.dev.tsv --guess $NAME.guess
 
 # is it feasible to do this on a training set? I believe so. The English europarl
 # set has 75k unique types (not too different from the task dev set. The
