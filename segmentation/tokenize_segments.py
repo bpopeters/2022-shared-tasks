16bf321234e7c740d3582745839eec75ba3be4df
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-26 11:59:12 +0000

    add second, smaller grid with smaller batch sizes and fyls

--- segmentation/tokenize_segments.py
+++ segmentation/tokenize_segments.py
@@ -1,3 +1,9 @@
+# This script should ONLY be used for word-level experiments because it
+# destroys whitespace.
+# However, I already ran a huge grid using data using a preprocessing pipeline
+# that included this script.
+# So, it gets to continue to exist for documentary purposes.
+
 import sys
 import re
 
