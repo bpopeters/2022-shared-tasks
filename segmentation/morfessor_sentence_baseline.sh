e6ea500ba61ea7a29e05a2dd6337a53f05b33ea5
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-28 14:34:39 +0100

    fix sentence-level preprocessing script

--- segmentation/morfessor_sentence_baseline.sh
+++ segmentation/morfessor_sentence_baseline.sh
@@ -12,16 +12,8 @@ morfessor-train -s $NAME.bin $TRAIN
 DEV=$NAME.dev.mor
 cut -f 1 $DATA.dev.tsv > $DEV
 
-# morfessor-segment is incredibly annoying because it outputs words on separate
-# lines. But we want to keep the original structure of the dev file!
-# First, we prepend @@ to every token except the first of each word
-morfessor-segment $DEV -l $NAME.bin | python label_suffixes.py > segments.tmp
+bash morfessor_segment_corpus.sh $NAME.bin $DEV > $NAME.mor.out
 
-python line_lengths.py < $DEV > lengths.tmp
-
-# then, we need to rebuild sentences from that.
-python rebuild_sentences.py lengths.tmp segments.tmp > $NAME.mor.out
-
-cut -f 1 $DATA.dev.tsv | paste - $NAME.mor.out > $NAME.mor.guess
+paste $DEV $NAME.mor.out > $NAME.mor.guess
 
 python 2022SegmentationST/evaluation/evaluate_word.py --gold $DATA.dev.tsv --guess $NAME.mor.guess
--- segmentation/tokenize_segments.py
+++ segmentation/tokenize_segments.py
@@ -9,10 +9,30 @@ import re
 
 
 def tokenize(line):
+    # this function regrettably destroys whitespace, which makes its continued
+    # use problematic.
+    # So, it is deprecated for future use. Remember to change it before you run
+    # more word-level experiments!
     line = re.sub(r"@@", "@", line)
     line = re.sub(r" ", "", line)
     return " ".join(list(line))
 
 
+def tokenize_sentence(line):
+    # except the pipe character occurs in the czech data
+    line = line.strip()  # to account for (target-side) whitespace problems
+    line = re.sub(r" @@", "|", line)
+    line = re.sub(r" ", "_", line)
+    return " ".join(list(line)) + "\n"
+
+
+name = sys.argv[1]
+sentence_level = name.endswith("sentence")
+if not sentence_level:
+    sys.stderr.write("WARNING! Deprecated tokenizer!\n")
+
 for line in sys.stdin:
-    sys.stdout.write(tokenize(line))
+    if sentence_level:
+        sys.stdout.write(tokenize_sentence(line))
+    else:
+        sys.stdout.write(tokenize(line))
