1745ff1e330de70632223c69f244c83742232929
bpopeters <bpeters@coli.uni-saarland.de>
2022-03-29 17:14:25 +0100

    fix dictionary reformatting

--- segmentation/make_spm_training_data.sh
+++ segmentation/make_spm_training_data.sh
@@ -24,6 +24,6 @@ cut -f 2 $DATA.dev.tsv > $OUT_DATA.dev.tgt.tmp
 cut -f 3 $DATA.train.tsv > $OUT_DATA.dev.tags.tmp
 spm_encode --model $OUT_DATA.spm.model --output_format piece < $OUT_DATA.dev.tgt.tmp | paste $OUT_DATA.dev.src.tmp - $OUT_DATA.dev.tags.tmp > $OUT_DATA.dev.tsv
 
-sed "s/\t/ /g" $OUT_DATA.spm.vocab | tail -n +4 > $OUT_DATA.fairseq.vocab
+tail -n +4 $OUT_DATA.spm.vocab | cut -f 1 | sed "s/$/ 100/g" > $OUT_DATA.fairseq.vocab
 
 rm $OUT_DATA.*.tmp
