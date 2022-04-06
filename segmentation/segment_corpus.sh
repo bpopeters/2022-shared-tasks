c5fbee98cd5b03d3a45681d66f15110acc9e3b38
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-06 13:01:23 +0100

    make yet another output file for debugging

--- segmentation/segment_corpus.sh
+++ segmentation/segment_corpus.sh
@@ -19,8 +19,8 @@ fairseq-interactive \
     --beam 5 \
     --alpha 1.5  \
     --batch-size 256 \
-    --buffer-size 256 < $EXPNAME.uniqs.tmp | \
-    grep -P '^H-'  | cut -c 3- | awk -F "\t" '{print $NF}' > $EXPNAME.uniqs.wtf
+    --buffer-size 256 < $EXPNAME.uniqs.tmp > $EXPNAME.uniqs.out
+    cat $EXPNAME.uniqs.out | grep -P '^H-'  | cut -c 3- | awk -F "\t" '{print $NF}' > $EXPNAME.uniqs.wtf
     python postprocess_fairseq.py $NAME < $EXPNAME.uniqs.wtf > $EXPNAME.uniqs.segmented
 
 # build the dictionary
