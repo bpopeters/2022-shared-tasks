568434efd5972e36df03801828fc63a4451424a8
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-06 11:46:41 +0100

    use nltk tokenizer when creating unique types

--- segmentation/unique_types.py
+++ segmentation/unique_types.py
@@ -2,14 +2,15 @@
 
 import sys
 import re
+import nltk
 
 
 def main():
     min_count = int(sys.argv[1]) if len(sys.argv) > 1 else 2
     uniqs = set()
     for line in sys.stdin:
-        line = re.sub(r'[\.,:;\'\"\(\)]', '', line)
-        uniqs.update(tok for tok in line.strip().split())
+        toks = nltk.word_tokenize(line)
+        uniqs.update(toks)
     for word in uniqs:
         if len(word) >= min_count:
             sys.stdout.write(" ".join(list(word)) + "\n")
