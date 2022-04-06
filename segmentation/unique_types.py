7adba017ed0f5f70a400f23128681f0352256acf
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-06 11:09:57 +0100

    have unique_types return everything with length greater than 1

--- segmentation/unique_types.py
+++ segmentation/unique_types.py
@@ -5,12 +5,13 @@ import re
 
 
 def main():
+    min_count = int(sys.argv[1]) if len(sys.argv) > 1 else 2
     uniqs = set()
     for line in sys.stdin:
         line = re.sub(r'[\.,:;\'\"\(\)]', '', line)
         uniqs.update(tok for tok in line.strip().split())
     for word in uniqs:
-        if len(word) > 3:
+        if len(word) >= min_count:
             sys.stdout.write(" ".join(list(word)) + "\n")
 
 
