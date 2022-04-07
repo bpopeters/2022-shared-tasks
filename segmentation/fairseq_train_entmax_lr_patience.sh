ea63b0d952e7a2b1e89955a293cda1711067f1bd
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-07 13:17:00 +0100

    add another training script

---
+++ segmentation/fairseq_train_entmax_lr_patience.sh
@@ -0,0 +1,61 @@
+readonly DATA=$1  # example: 2022-shared-tasks/segmentation/eng.word
+NAME=$( basename $DATA )  # i.e. eng.word
+EMB=$2
+HID=$3
+LAYERS=$4
+DROPOUT=$5
+BATCH=$6
+ENTMAX_ALPHA=$7
+
+# Adapted from the SIGMORPHON 2020 script by Kyle Gorman and Shijie Wu.
+
+set -euo pipefail
+
+# Defaults.
+readonly SEED=1917
+readonly CRITERION=entmax_loss
+readonly OPTIMIZER=adam
+readonly LR=1e-3
+readonly CLIP_NORM=1.
+readonly MAX_UPDATE=50000
+readonly SAVE_INTERVAL=1
+readonly SCHEDULER=reduce_lr_on_plateau
+readonly PATIENCE=10
+readonly LR_PATIENCE=5
+
+MODEL_DIR="fairseq-checkpoints/lr-patience/${NAME}-entmax-${EMB}-${HID}-${LAYERS}-${DROPOUT}-${BATCH}-${ENTMAX_ALPHA}"
+
+train() {
+    local -r CP="$1"; shift
+    fairseq-train \
+        "data-bin/${NAME}" \
+        --save-dir="${CP}" \
+        --source-lang="${NAME}.src" \
+        --target-lang="${NAME}.tgt" \
+        --seed="${SEED}" \
+        --arch=lstm \
+        --encoder-bidirectional \
+        --encoder-layers "${LAYERS}" \
+        --decoder-layers "${LAYERS}" \
+        --dropout="${DROPOUT}" \
+        --encoder-embed-dim="${EMB}" \
+        --encoder-hidden-size="${HID}" \
+        --decoder-embed-dim="${EMB}" \
+        --decoder-out-embed-dim="${EMB}" \
+        --decoder-hidden-size="${HID}" \
+        --share-decoder-input-output-embed \
+        --criterion="${CRITERION}" \
+        --loss-alpha="${ENTMAX_ALPHA}" \
+        --optimizer="${OPTIMIZER}" \
+        --lr="${LR}" \
+        --lr-scheduler="${SCHEDULER}" \
+        --clip-norm="${CLIP_NORM}" \
+        --batch-size="${BATCH}" \
+        --max-update="${MAX_UPDATE}" \
+        --save-interval="${SAVE_INTERVAL}" \
+        --patience="${PATIENCE}" \
+        --no-epoch-checkpoints \
+        "$@"   # In case we need more configuration control.
+}
+
+train $MODEL_DIR
--- segmentation/train_000_tagger.py
+++ segmentation/train_000_tagger.py
@@ -12,6 +12,7 @@ from itertools import groupby
 import pickle
 
 import numpy as np
+import torch
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
@@ -72,10 +73,47 @@ def read_tsv(path, category):
     return data
 
 
+def ngram_features(data, n):
+    cv = CountVectorizer(
+            input='content', analyzer='char',
+            lowercase=True, ngram_range=(1, opt.n)
+    )
+    X = cv.fit_transform(data["word"])
+    return X
+
+
+def batches(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+# so, this seems to work but it's a lot of compute
+# Maybe I should write my own classifier module in torch (i.e. solution code)
+# and use flair for the features. That might be smarter.
+def flair_features(data, batch_size=128):
+    import flair
+    forward_emb = flair.embeddings.FlairEmbeddings('news-forward')
+    backward_emb = flair.embeddings.FlairEmbeddings('news-backward')
+    stacked_emb = flair.embeddings.StackedEmbeddings([forward_emb, backward_emb])
+    # problem: what if there are words with a space in the middle? Maybe pool.
+    sentences = [flair.data.Sentence(word) for word in data["word"]]
+
+    # split the sentence into batches
+    for i, batch in enumerate(batches(sentences, batch_size), 1):
+        print("Embedding batch {}".format(i))
+        stacked_emb.embed(batch)
+
+    X = torch.stack([torch.stack([word.embedding for word in sent]).max(dim=0)[0]
+                     for sent in sentences[:50]])
+    return X.numpy()
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('data')
     parser.add_argument('out')
+    parser.add_argument('-features', default="ngram", choices=["ngram", "flair"])
     parser.add_argument('-n', type=int, default=3)
     opt = parser.parse_args()
 
@@ -84,17 +122,17 @@ def main():
     # construct examples (column 0) and labels (binarize as label == "000")
     # evaluate on dev: is it any good?
     # build ngram features for words
-    cv = CountVectorizer(
-            input='content', analyzer='char',
-            lowercase=True, ngram_range=(1, opt.n)
-    )
-    X = cv.fit_transform(data["word"])
+    if opt.features == "ngram":
+        X, embedder = ngram_features(data, opt.n)
+    else:
+        X = flair_features(data)
     y = np.array([label == "000" for label in data["category"]], dtype=int)
     clf = LogisticRegression(max_iter=1000)
-    # print(cross_val_score(clf, X, y, cv=5))
-    clf.fit(X, y)
+    print(cross_val_score(clf, X, y, cv=5))
+    # clf.fit(X, y)
     # clf.sparsify()
-    Tagger(clf, cv).save(opt.out)
+    # this last part will break currently with flair
+    Tagger(clf, embedder).save(opt.out)
 
 
 if __name__ == '__main__':
