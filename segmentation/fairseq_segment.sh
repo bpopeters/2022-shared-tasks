d74138779138af167c5674ef85f26bb787b5d397
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-14 11:30:59 +0100

    refactor segmentation

--- segmentation/fairseq_segment.sh
+++ segmentation/fairseq_segment.sh
@@ -1,9 +1,9 @@
-readonly DATA=$1  # example: 2022-shared-tasks/data/eng.word
+readonly DATA_BIN=$1
 NAME=$( basename $DATA )  # i.e. eng.word
 readonly MODEL_PATH=$2
 readonly ENTMAX_ALPHA=$3
-
-readonly BEAM=5
+readonly BEAM=$4
+readonly GOLD_PATH=$5
 
 decode() {
     local -r CP="$1"; shift
@@ -11,13 +11,13 @@ decode() {
     # Fairseq insists on calling the dev-set "valid"; hack around this.
     local -r FAIRSEQ_MODE="${MODE/dev/valid}"
     CHECKPOINT="${CP}/checkpoint_best.pt"
-    OUT="${CP}/${MODE}.out"
-    PRED="${CP}/${MODE}.pred"
+    OUT="${CP}/${MODE}-${BEAM}.out"
+    PRED="${CP}/${MODE}-${BEAM}.pred"
     # Makes raw predictions.
     fairseq-generate \
-        "data-bin/${NAME}" \
-        --source-lang="${NAME}.src" \
-        --target-lang="${NAME}.tgt" \
+        "${DATA_BIN}" \
+        --source-lang="src" \
+        --target-lang="tgt" \
         --path="${CHECKPOINT}" \
         --gen-subset="${FAIRSEQ_MODE}" \
         --beam="${BEAM}" \
@@ -25,11 +25,10 @@ decode() {
 	--batch-size 256 \
         > "${OUT}"
     # Extracts the predictions into a TSV file.
-    cat "${OUT}" | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | python scripts/postprocess_fairseq.py $NAME > $PRED
-    cut -f 1 "${DATA}.dev.tsv" | paste - $PRED > "${CP}/${MODE}.guess"
+    cat "${OUT}" | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | python postprocess.py > $PRED
+    cut -f 1 "${DATA}.dev.tsv" | paste - $PRED > "${CP}/${MODE}-${BEAM}.guess"
     # Applies the evaluation script to the TSV file.
-    python 2022SegmentationST/evaluation/evaluate.py --gold "${DATA}.dev.tsv" --guess "${CP}/${MODE}.guess" > "${CP}/${MODE}.results"
-    # python 2022SegmentationST/evaluation/evaluate.py --gold "${DATA}.dev.tsv" --guess "${CP}/${MODE}.guess" --category > "${CP}/${MODE}.tagged.results"
+    python 2022SegmentationST/evaluation/evaluate.py --gold $GOLD_PATH --guess "${CP}/${MODE}.guess" > "${CP}/${MODE}-${BEAM}.results"
 }
 
 decode $MODEL_PATH dev
---
+++ segmentation/postprocess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+
+def postprocess_chars(line):
+    line = "".join(line.strip().split(" "))
+    line = line.replace("_", " ")
+    line = line.replace("|", " @@")
+    return line
+
+
+def main(args):
+    for line in sys.stdin:
+        sys.stdout.write(postprocess_chars(line))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--spm-model", default=None)
+    opt = parser.parse_args()
+    main(opt)
