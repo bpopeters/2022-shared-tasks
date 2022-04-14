73cdbe7a6caac4d84e0bfef31463fe0376b02bb8
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-14 16:36:06 +0100

    do not hard code batch sizes in grid

--- segmentation/xe_grid.sh
+++ segmentation/xe_grid.sh
@@ -4,6 +4,7 @@
 
 DATA_BIN=$1  # e.g. "new-data-bin/char2piece/ces.word-1000"
 NAME=$2  # e.g. "ces-v1000-2022-04-13"
+shift 2
 
 DROPOUT=0.3
 EMB=512
@@ -11,7 +12,7 @@ ALPHA=1.5
 
 for HID in 512 1024 ; do
     for LAYERS in 1 2 ; do
-        for BATCH in 32 64 ; do
+        for BATCH in $@ ; do
             for LR in 0.001 0.0005 0.0001 ; do
                 bash fairseq_train_xe_improved.sh $DATA_BIN $NAME $EMB $HID $LAYERS $BATCH $LR
             done
