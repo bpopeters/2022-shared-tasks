db546460a1c6a4359d752ec5c7bf71f7026ca3ac
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-21 14:26:33 +0100

    add grid for sentences that omits lowest learning rate

---
+++ segmentation/entmax_sentence_minlev_grid.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# A more conventional seq2seq baseline for Czech.
+
+DATA_BIN=$1  # e.g. "new-data-bin/char2piece/ces.word-1000"
+NAME=$2  # e.g. "ces-v1000-2022-04-13"
+GOLD_PATH=$3
+GRID_LOC=$4
+shift 4
+
+DROPOUT=0.3
+EMB=512
+ALPHA=1.5
+
+for HID in 512 1024 ; do
+    for LAYERS in 1 2 ; do
+        for BATCH in $@ ; do
+            for LR in 0.001 0.0005 ; do
+                bash fairseq_train_minlev.sh $DATA_BIN $NAME $EMB $HID $LAYERS $BATCH $ALPHA $LR $GRID_LOC
+                bash fairseq_segment.sh $DATA_BIN "${GRID_LOC}/${NAME}-entmax-minlev-${EMB}-${HID}-${LAYERS}-${BATCH}-${ALPHA}-${LR}" 1.5 5 $GOLD_PATH
+            done
+        done
+    done
+done
