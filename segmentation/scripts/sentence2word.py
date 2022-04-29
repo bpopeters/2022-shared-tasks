1599dca1fba427b3bf11549f97666448439e9f2b
bpopeters <bpeters@coli.uni-saarland.de>
2022-04-29 09:34:35 +0100

    add context option for sentence2word

--- segmentation/scripts/sentence2word.py
+++ segmentation/scripts/sentence2word.py
@@ -2,6 +2,7 @@
 
 # take a sentence-level corpus and turn it into a word-level corpus
 
+import argparse
 import sys
 
 def get_segmented_words(segment_seq):
@@ -17,20 +18,38 @@ def get_segmented_words(segment_seq):
     return [" ".join(word) for word in words]
 
 
+def add_context_marker(surface_seq):
+    surface_toks = surface_seq.split()
+    # for each token in surface_toks
+    ret = []
+    for i, tok in enumerate(surface_toks):
+        labeled_seq = [t if i != j else "<p> {} </p>".format(t)
+                       for j, t in enumerate(surface_toks)]
+        labeled_string = " ".join(labeled_seq)
+        ret.append(labeled_string)
+    return ret
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--context", action="store_true")
+    opt = parser.parse_args()
     for line in sys.stdin:
         surface_seq, segment_seq = line.strip().split("\t")
         # these two lines because of problems in the data:
         surface_seq = surface_seq.strip()
         segment_seq = segment_seq.strip()
 
-        surface_toks = surface_seq.split()
+        if opt.context:
+            src_toks = add_context_marker(surface_seq)
+        else:
+            src_toks = surface_seq.split()
         segmented_words = get_segmented_words(segment_seq)
-        if len(surface_toks) == len(segmented_words):
-            for word, word_segments in zip(surface_toks, segmented_words):
+        if len(src_toks) == len(segmented_words):
+            for word, word_segments in zip(src_toks, segmented_words):
                 sys.stdout.write("\t".join([word, word_segments]) + "\n")
         else:
-            sys.stderr.write(str(surface_toks) + "\t" + str(segmented_words) + "\n")
+            sys.stderr.write(str(src_toks) + "\t" + str(segmented_words) + "\n")
         
             
 
