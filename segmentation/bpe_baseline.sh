4cf2e8514c779928bd8bdecef7d9514eba970013
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-01 23:18:52 +0100

    update bpe baseline

--- segmentation/bpe_baseline.sh
+++ segmentation/bpe_baseline.sh
@@ -1,52 +1,28 @@
 DATA=$1  # no "train.tsv" or "dev.tsv" suffix
 VOCAB=$2
+COL=$3
 
 NAME=$( basename $DATA )
 
+EXP_NAME="unsup-baselines/bpe/${NAME}-${VOCAB}-${COL}"
+
+mkdir -p $EXP_NAME
+
 # copy unsegmented column to a temporary file
-TRAIN=$NAME.train.tmp
-cut -f 1 $DATA.train.tsv > $TRAIN
+TRAIN="${EXP_NAME}/train.tmp"
+cut -f $COL $DATA.train.tsv > $TRAIN
 
 # train spm model
-spm_train --input $TRAIN --model_prefix spm.$VOCAB --vocab_size $VOCAB --character_coverage 1.0 --model_type bpe
+spm_train --input $TRAIN --model_prefix $EXP_NAME/spm.$VOCAB --vocab_size $VOCAB --character_coverage 1.0 --model_type "bpe"
 
 # segment dev set
-DEV=$NAME.dev.tmp
+DEV=$EXP_NAME/dev.tmp
 cut -f 1 $DATA.dev.tsv > $DEV
-spm_encode --model spm.$VOCAB.model --output_format piece < $DEV | python scripts/spm2out.py > $NAME.spm.out
+spm_encode --model $EXP_NAME/spm.$VOCAB.model --output_format piece < $DEV | python scripts/spm2out.py > "${EXP_NAME}/spm.out"
 
 # evaluate
-paste $DEV $NAME.spm.out > guess
+paste $DEV "${EXP_NAME}/spm.out" > "${EXP_NAME}/guess"
 
-python 2022SegmentationST/evaluation/evaluate.py --gold $DATA.dev.tsv --guess guess
+python 2022SegmentationST/evaluation/evaluate.py --gold $DATA.dev.tsv --guess "${EXP_NAME}/guess" > "${EXP_NAME}/dev.results"
 
-rm guess
-rm *.tmp
-
-# copy unsegmented:
-# 48790
-# 57433
-
-# 4000:
-# 51771
-# 57433 eng.word.dev.tmp.out
-
-# 8000:
-# 49105
-# 57433 eng.word.dev.tmp.out
-
-# 16000:
-# 47002
-# 57433 eng.word.dev.tmp.out
-
-# 32000:
-# 45832
-# 57433 eng.word.dev.tmp.out
-
-# 64000:
-# 45226
-# 57433 eng.word.dev.tmp.out
-
-#128000:
-# 45341
-# 57433 eng.word.dev.tmp.out
+rm "${EXP_NAME}/"*".tmp"
