d4369c15aca47c665dec22f0c010a5cee86f0f5d
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-03 18:39:22 +0100

    do not mess up morpheme separation

--- segmentation/smart_downsample.py
+++ segmentation/smart_downsample.py
@@ -16,18 +16,12 @@ def read_tsv(path, category):
         for line in f:
             fields = line.rstrip("\n").split("\t")
             for name, field in zip(col_names, fields):
-                if name == "segments":
-                    field = field.replace(' @@', '|')
-                    field = field.replace(' ', '|')
                 data[name].append(field)
     return data
 
 
 def main(args):
     train = read_tsv(args.train, True)
-    train_morphemes = Counter(
-        chain.from_iterable([seg.split("|") for seg in train["segments"]])
-    )
     # what do I want? a new dataset that covers all the same morphemes as the
     # original one, but that is smaller.
     # map from examples to the morphemes they contain
@@ -35,7 +29,7 @@ def main(args):
     morph2i = defaultdict(list)
     i2morph = []
     for i, t in enumerate(train["segments"]):
-        ex_morphemes = set(t.split("|"))  # does it matter that this is a set?
+        ex_morphemes = set(t.split(" @@"))  # does it matter that this is a set?
         i2morph.append(ex_morphemes)
         for m in ex_morphemes:
             morph2i[m].append(i)
