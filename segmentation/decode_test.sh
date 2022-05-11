33c8567d342e7beac41f93ab268a4a0cab8f798c
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-11 12:33:12 +0100

    add beginning of test script

---
+++ segmentation/decode_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+DATA_PATH=$1
+MODEL_PATH=$2
+TEST_PATH=$3
+shift 3
+
+# todo: add case for character-level segmentation
+preptok() {
+    cat $1 | spm_encode --model "${DATA_PATH}/src.model"
+}
+
+# tokenize test examples: this is either whitespace segmentation or applying
+# a sentencepiece model to them
+
+preptok $TEST_PATH | \
+    fairseq-interactive \
+        $DATA_PATH \
+        --path $MODEL_MODEL \
+        --source-lang src \
+        --target-lang tgt \
+        --beam 5 \
+        --alpha 1.5  \
+        --batch-size 256 \
+        --buffer-size 256
+    
