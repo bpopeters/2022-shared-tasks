8f3ad49c83c1420b5507d6e99603d31a225cb8a4
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-19 16:44:16 +0100

    add more scripts for analysis

---
+++ segmentation/scripts/build_vocab_for_analysis.py
@@ -0,0 +1,14 @@
+from .tokenize import read_tsv, prepare_spm
+
+
+def main(corpus, vocab, spm_path, *kwargs):
+    vocab = int(vocab)
+    data = read_tsv(args.corpus)
+    src = data["surface"]
+    tgt = data["segment"]
+
+    prepare_spm(None, spm_path, chain(src, tgt), vocab)
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
---
+++ segmentation/spm_baseline_both_cols.sh
@@ -0,0 +1,20 @@
+TRAIN=$1
+DEV=$2
+VOCAB=$3
+
+NAME=$( basename $TRAIN .train.tsv)
+
+EXP_NAME="unsup-baselines/spm/both-cols/${NAME}-${VOCAB}"
+
+mkdir -p $EXP_NAME
+
+# train spm model
+python scripts/build_vocab_for_analysis.py $TRAIN $VOCAB $EXP_NAME/spm.$VOCAB
+
+# segment dev set
+cut -f 1 $DEV | spm_encode --model $MODEL --output_format piece | python scripts/spm2out.py > "${EXP_NAME}/spm.out"
+
+# evaluate
+cut -f 1 $DEV | paste - "${EXP_NAME}/spm.out" > "${EXP_NAME}/guess"
+
+python 2022SegmentationST/evaluation/evaluate.py --gold $DEV --guess "${EXP_NAME}/guess" > "${EXP_NAME}/dev.results"
