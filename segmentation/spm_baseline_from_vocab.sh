e3b68ecb17129d88120beb942903ab8255188dec
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-19 14:26:42 +0100

    train spm baseline from DeepSPIN-3 models

---
+++ segmentation/spm_baseline_from_vocab.sh
@@ -0,0 +1,17 @@
+MODEL=$1
+DATA=$2 # full path
+GOLD=$3
+
+NAME=$( basename $DATA .dev.tsv)
+
+EXP_NAME="unsup-baselines/spm/piece2piece-vocab/${NAME}"
+
+mkdir -p $EXP_NAME
+
+# segment dev set
+cut -f 1 $DATA | spm_encode --model $MODEL --output_format piece | python scripts/spm2out.py > "${EXP_NAME}/spm.out"
+
+# evaluate
+cut -f 1 $DATA | paste - "${EXP_NAME}/spm.out" > "${EXP_NAME}/guess"
+
+python 2022SegmentationST/evaluation/evaluate.py --gold $DATA.dev.tsv --guess "${EXP_NAME}/guess" > "${EXP_NAME}/dev.results"
