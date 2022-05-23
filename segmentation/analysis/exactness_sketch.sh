aec0825c9b1c3e0234f4c5066eb12f305fa25d73
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-23 18:08:00 +0100

    add script for computing beam exactness

---
+++ segmentation/analysis/exactness_sketch.sh
@@ -0,0 +1,22 @@
+DATA_BIN=$1
+MODEL_PATH=$2
+BEAM="${3}"
+
+MODEL_DIR=$( dirname $MODEL_PATH )
+
+
+cat $DATA_BIN/dev.src | \
+    fairseq-interactive \
+        $DATA_BIN \
+        --path $MODEL_PATH \
+        --source-lang src \
+        --target-lang tgt \
+        --alpha 1.5 \
+        --batch-size 256 \
+        --buffer-size 256 \
+        --nbest $BEAM \
+        --beam $BEAM \
+        --unnormalized | \
+        grep -P '^H-' | python scripts/compute_beam_exactness.py \
+        > "${MODEL_DIR}/dev-${BEAM}.exactness"
+
