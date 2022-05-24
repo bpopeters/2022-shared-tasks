c4a8bee28949ff4fc1278b02d08f924c8543838a
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-24 15:14:04 +0100

    update exactness sketch

--- segmentation/analysis/exactness_sketch.sh
+++ segmentation/analysis/exactness_sketch.sh
@@ -1,6 +1,7 @@
 DATA_BIN=$1
 MODEL_PATH=$2
 BEAM="${3}"
+shift 3
 
 MODEL_DIR=$( dirname $MODEL_PATH )
 
@@ -11,12 +12,11 @@ cat $DATA_BIN/dev.src | \
         --path $MODEL_PATH \
         --source-lang src \
         --target-lang tgt \
-        --alpha 1.5 \
-        --batch-size 256 \
         --buffer-size 256 \
         --nbest $BEAM \
         --beam $BEAM \
         --unnormalized | \
+        "$@" | \
         grep -P '^H-' | python scripts/compute_beam_exactness.py \
         > "${MODEL_DIR}/dev-${BEAM}.exactness"
 
