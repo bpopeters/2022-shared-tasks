ac0ce43ab7ea318eee443882d723aff548f7f176
bpopeters <bpeters@coli.uni-saarland.de>
2022-05-24 06:37:27 +0100

    add column for whether beam has certainly found the argmax

--- segmentation/scripts/compute_beam_exactness.py
+++ segmentation/scripts/compute_beam_exactness.py
@@ -5,16 +5,23 @@ from collections import defaultdict
 from itertools import groupby
 
 # input: sequence of hypothesis lines from output of fairseq-(generate|interactive)
+# output: currently a sum for hypotheses.
+# I would like to add another column.
 
 
 def main():
     # would prefer to do it in streaming fashion, though
     for label, example in groupby(sys.stdin, lambda x: x.split("\t")[0]):
         total_prob = 0.0
+        best_hyp = 0.0
         for hyp_line in example:
             _, score, hyp = hyp_line.strip().split("\t")
-            total_prob += 2 ** float(score)
-        sys.stdout.write("\t".join([label, str(total_prob)]) + "\n")
+            hyp_prob = 2 ** float(score)
+            total_prob += hyp_prob
+            best_hyp = max(best_hyp, hyp_prob)
+        remaining_mass = 1 - total_prob
+        argmax_cert = str(int(best_hyp > remaining_mass))
+        sys.stdout.write("\t".join([label, str(total_prob), argmax_cert]) + "\n")
 
     '''
     for line in sys.stdin:
