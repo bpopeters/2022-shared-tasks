6c395e10983f23f6587b59f33488fb8150812abe
bpopeters <bpeters@coli.uni-saarland.de>
2022-06-01 10:25:20 +0100

    czech config with smaller valid batch size

---
+++ segmentation/entmax_transformer_grid_ces_char.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DATA_BIN=$1  # e.g. "new-data-bin/char2piece/ces.word-1000"
+NAME=$2  # e.g. "ces-v1000-2022-04-13"
+GOLD_PATH=$3
+GRID_LOC=$4
+shift 4
+
+ENTMAX_ALPHA=1.5
+LR=0.001
+
+grid() {
+    local -r EMB="$1"; shift
+    local -r HID="$1"; shift
+    local -r LAYERS="$1" ; shift
+    local -r HEADS="$1" ; shift
+    for WARMUP in 4000 8000 ; do
+        for DROPOUT in 0.1 0.3 ; do
+            for BATCH in $@ ; do
+                MODEL_DIR="${GRID_LOC}/${NAME}-entmax-minlev-${EMB}-${HID}-${LAYERS}-${HEADS}-${BATCH}-${ENTMAX_ALPHA}-${LR}-${WARMUP}-${DROPOUT}"
+                if [ ! -f "${MODEL_DIR}/dev-5.results" ]
+                then
+                    bash fairseq_train_entmax_transformer.sh $DATA_BIN $NAME $EMB $HID $LAYERS $HEADS $BATCH $ENTMAX_ALPHA $LR $WARMUP $DROPOUT $GRID_LOC --max-tokens-valid 4096
+                    bash fairseq_segment.sh $DATA_BIN $MODEL_DIR 1.5 5 $GOLD_PATH
+                else
+                    echo "skipping ${MODEL_DIR}"
+                fi
+            done
+        done
+    done
+}
+
+
+grid 256 1024 6 8 $@
+grid 512 2048 6 8 $@
